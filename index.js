e0aae6212eaff0e42a3202aa2efc3b95d68f0e3e
SwatiPriya2805 <swatipriya2805@gmail.com>
2022-01-06 19:35:34 +0530

    wa-share icon

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(`${__dirname}/button.js`));
+
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err.message);
+    }
+  });
+});
+app.get("/button.js", (req, res) => {
+  res.sendFile(`${__dirname}/button.js`, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err.message);
+    }
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
